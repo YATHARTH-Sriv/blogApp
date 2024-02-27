0dab65914a11b1345d098fb80e720c53cdf96dc1
yatharth srivastava <yatharthsriv18@gmail.com>
2024-02-27 13:02:54 +0530

    setting and adding values to env files

---
+++ BlogApp/src/conf.js
@@ -0,0 +1,8 @@
+const conf={
+    appwrite_url:String(import.meta.env.VITE_APPWRITE_URL),
+    appwrite_project_id:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwrite_DATABASE_ID:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwrite_COLLECTIONID:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBUCKETID:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
+}
